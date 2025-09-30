478ec14b19b2b80baabc68085e6821e96d1abb0d
mounikanv <mounikanattama5b3@gmail.com>
2025-09-30 22:45:27 +0530

    roboshop-instances

--- roboshopinstances.sh
+++ roboshopinstances.sh
@@ -6,8 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z03206712GAA20NO8JN0L" # replace with your ZONE ID
 DOMAIN_NAME="mounika.site" # replace with your domain
 
-#  for instance in ${INSTANCES[@]}
 
+#for instance in ${INSTANCES[@]}
 for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-04191353100073bf2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
@@ -20,7 +20,6 @@ do
         RECORD_NAME="$DOMAIN_NAME"
     fi
     echo "$instance IP address: $IP"
-    
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
